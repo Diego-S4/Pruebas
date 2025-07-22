78000e3860990c10cf83587973bccb85f5c93254
Diego-S4 <sandovalparedesdiego444@gmail.com>
2025-07-22 11:14:19 -0500

    ....

--- public/js/contacto.js
+++ public/js/contacto.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     new QRCode(document.getElementById('qrcode-contacto'), {
-        text: 'https://wa.me/51987654321',
+        text: 'https://wa.me/51955696928',
         width: 128,
         height: 128
     });
